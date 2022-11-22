64b9fa8420eedae14079056392167b5767157bc7
akhdz <117084221+akhdz@users.noreply.github.com>
2022-11-22 11:34:28 +0700

    Add files via upload

--- tgapp.py
+++ tgapp.py
@@ -1,15 +1,29 @@
 from tg import expose, TGController
 
 class RootController(TGController):
+
     @expose()
     def index(self):
         return 'Hello Word, by PABW 7B2 Punokawan'
+    
+    @expose('json.xhtml')
+    def json(self, person=None):
+        return dict(person=person)
+
+    @expose('xml.xhtml')
+    def xml(self, person=None):
+        return dict(person=person)
+
+    @expose('csv.xhtml')
+    def csv(self, person=None):
+        return dict(person=person)
 
 from tg import MinimalApplicationConfigurator
 
 config = MinimalApplicationConfigurator()
 config.update_blueprint({
-    'root_controller': RootController()
+    'root_controller': RootController(),
+    'renderers': ['kajiki']
 })
 
 application = config.make_wsgi_app()
